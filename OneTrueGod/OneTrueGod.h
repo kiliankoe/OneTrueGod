35ddb9b12f1d5462913d80f6c28ff7fbe2f1f68e
Kilian Koeltzsch <me@kilian.io>
2017-11-06 22:40:30 +0100

    one true god

---
+++ OneTrueGod/OneTrueGod.h
@@ -0,0 +1,19 @@
+//
+//  OneTrueGod.h
+//  OneTrueGod
+//
+//  Created by Kilian Költzsch on 06.11.17.
+//  Copyright © 2017 Kilian Koeltzsch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OneTrueGod.
+FOUNDATION_EXPORT double OneTrueGodVersionNumber;
+
+//! Project version string for OneTrueGod.
+FOUNDATION_EXPORT const unsigned char OneTrueGodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OneTrueGod/PublicHeader.h>
+
+
---
+++ README.md
@@ -0,0 +1,9 @@
+# One True God
+
+Be sure to add the following to your Cartfile.
+
+```
+github "kiliankoe/OneTrueGod" ~> 1.0
+```
+
+🙌
